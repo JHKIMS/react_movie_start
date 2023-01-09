ce77505257cb54fc2797d4413ffa157db317362b
JHKIMS <wjdgns5131@gmail.com>
2023-01-09 14:32:16 +0900

    ReactStart : UseEffect사용법 정리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import Button from "./Button.js"
-import styles from "./App.module.css"
+// import Button from "./Button.js"
+// import styles from "./App.module.css"
+import {useState, useEffect} from "react"
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prevCount) => prevCount+1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("I run all the time");
+  useEffect(() =>{
+    console.log("Only One Call Api")
+  }, []);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length>5){
+      console.log("Search For", keyword);
+    }
+  },[keyword]);
+  useEffect(() =>{
+    console.log("I run when 'counter' changes");
+  },[counter]);
+  useEffect(() =>{
+    console.log("counter or keyword changes");
+  }, [keyword, counter])
   return (
     <div>
-      <h1 className={styles.title}>Welcome React</h1>
-      <Button text={"Continue"}/>
+      {/* <h1 className={styles.title}>Welcome React</h1>
+      <Button text={"Continue"}/> */}
+
+
+      <input 
+      value={keyword}
+      onChange={onChange} type="text" placeholder="Search here..." />
+      
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
