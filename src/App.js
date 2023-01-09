20c811ca52e3511bb1131eea639bdcc353abf83c
JHKIMS <wjdgns5131@gmail.com>
2023-01-09 18:49:34 +0900

    ReactStart : Coin정보 보기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import Button from "./Button.js"
 // import styles from "./App.module.css"
-import {useState} from "react"
+import { useState, useEffect } from "react";
 
 // function Hello(){
 //   // function byFn() {
@@ -45,11 +45,10 @@ import {useState} from "react"
 //       {/* <h1 className={styles.title}>Welcome React</h1>
 //       <Button text={"Continue"}/> */}
 
-
-//       <input 
+//       <input
 //       value={keyword}
 //       onChange={onChange} type="text" placeholder="Search here..." />
-      
+
 //       <h1>{counter}</h1>
 //       <button onClick={onClick}>Click me</button>
 
@@ -59,8 +58,9 @@ import {useState} from "react"
 //   );
 // }
 
-// Todo만드는 App
-function App(){
+function App() {
+  /**
+     * Todo만드는 App
     const [toDo, setToDo] = useState("");
     const [toDos, setToDos] = useState([]);
     const onChange = (event) => setToDo(event.target.value);
@@ -86,6 +86,33 @@ function App(){
                     <li key={index}>{item}</li>))}
             </ul>
            </div>
+    */
+
+
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? (<strong>Loading...</strong>) : (
+        <select>
+        {coins.map((coin, index) => 
+            <option key={index}>
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>)}
+        </select>
+      )}
+        
+    </div>
+  );
 }
 
 export default App;
