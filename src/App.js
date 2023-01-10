b6c3e81437b1d6843bcf41621ecbcc815adb9d2b
JHKIMS <wjdgns5131@gmail.com>
2023-01-10 12:20:48 +0900

    ReactStart : Router만들기(+Todo, Coin 컴포넌트화)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 // import Button from "./Button.js"
 // import styles from "./App.module.css"
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./routes/Home.js";
+import Detail from "./routes/Detail.js";
+import Coin from "./components/Coin.js";
+import Todo from "./components/Todo.js";
 
 // function Hello(){
 //   // function byFn() {
@@ -59,63 +63,25 @@
 // }
 
 function App() {
-  return null;
-  /**
-     * Todo만드는 App
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault(); 
-        if(toDo === "" ){
-            return;
-        }
-        
-        setToDos(currentArray => [toDo, ...currentArray])
-        setToDo("");
-    }
-    return <div>
-            <h1>ToDoList{toDos.length}</h1>
-            <form onSubmit={onSubmit}>
-                <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/>
-                <button>Add To Do</button>
-            </form>
-
-            <hr />
-            <ul>
-             {toDos.map((item, index)=>( 
-                    <li key={index}>{item}</li>))}
-            </ul>
-           </div>
-    */
-
-  /**
-   * 코인리스트
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? (<strong>Loading...</strong>) : (
-        <select>
-        {coins.map((coin, index) => 
-            <option key={index}>
-                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>)}
-        </select>
-      )}
-        
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/Todo">
+          <Todo />
+        </Route>
+        <Route path="/coin">
+          <Coin />
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
-   */
+
 }
 
 export default App;
