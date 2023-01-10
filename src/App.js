6895fc20bee9c1ead14119e9cf42ed56c7db6dc7
JHKIMS <wjdgns5131@gmail.com>
2023-01-10 09:56:23 +0900

    ReactStart : 영화앱만들기_1

--- src/App.js
+++ src/App.js
@@ -88,7 +88,8 @@ function App() {
            </div>
     */
 
-
+  /**
+   * 코인리스트
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
   useEffect(() => {
@@ -113,6 +114,47 @@ function App() {
         
     </div>
   );
+   */
+
+  /** 영화앱 */
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  
+  const getMovies = async() => {
+    const json = await(
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+      setMovies(json.data.movies);
+      setLoading(false);
+  };
+  useEffect(() => {
+    getMovies()
+  },[]);
+/** 위 async await코드와 밑의 코드는 동일한 역할을 한다. 
+ * useEffect(() => {
+    fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    .then((response) => response.json())
+    .then((json) => setMovies(json.data.movies));
+  }, []); 
+*/ 
+
+  return <div>{loading ? ( <h1>Loading...</h1> ) : 
+  <div>
+    {movies.map((movie) => (
+      <div key={movie.id}>
+        <img src={movie.medium_cover_image}/>
+        <h2>{movie.title}</h2>
+        <p>{movie.summary}</p>
+        <ul>
+          {movie.genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>))}
+  </div>}
+      </div>
 }
 
 export default App;
